bc1caeae15ab58d313fb4fc11a4cddb19daa0ba3
david mark <markkariuki17@gmail.com>
2017-12-11 11:52:37 +0300

    addd change to the displayed links

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
 
 
     NEWS_API_KEY = 'e2213e59a10643e1a9bfd7cdf3ee86b9'
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources/{}?apiKey{}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v1/source'
 
 class ProdConfig(Config):
     pass
